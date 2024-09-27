e806da619aec273b5803b3d2cb26eff54bc3e62d
linzhangyu <gebi.lzy@raycloud.com>
2024-09-27 17:46:38 +0800

    feat: 初步完成博客

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://main--zy-my-blog.netlify.app/"
+});
